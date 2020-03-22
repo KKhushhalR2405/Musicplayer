844edf6232f6cdc12c775ed008df396a5edf698e
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2020-03-22 13:57:11 +0530

    musicplayer file

--- music.py
+++ music.py
@@ -4,3 +4,8 @@ from playsound import playsound
 
 playsound("file1.mp3")
 
+
+
+printf("Koyya Khushhal Reddy \n 18BIT0233 \n");
+cout << "Koyya Khushhal Reddy" << endl << "18BIT0233" << endl;
+5144286984
